f319ac4269a4071bc8899e83bd578345e7df6c15
Ciaraerica23 <ciaraestafford@gmail.com>
2023-03-01 16:54:06 -0500

    create Band and Musician model and creat connection and tests

--- index.test.js
+++ index.test.js
@@ -2,9 +2,6 @@ const {sequelize} = require('./db');
 const {Band, Musician} = require('./index')
 
 describe('Band and Musician Models', () => {
-    /**
-     * Runs the code prior to all tests
-     */
     beforeAll(async () => {
         // the 'sync' method will create tables based on the model class
         // by setting 'force:true' the tables are recreated each time the 
@@ -12,13 +9,16 @@ describe('Band and Musician Models', () => {
         await sequelize.sync({ force: true });
     })
 
-    test('can create a Band', async () => {
-        // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+    test('can create a Band', async ()=> {
+        const band1 = await Band.create({ name: 'George', instrument:"guitar"});
+        expect(band1.name).toBe('George');
+        expect(band1.instrument).toBe('guitar');
     })
 
-    test('can create a Musician', async () => {
-        // TODO - test creating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+    test('can create a Musician', async() => {
+        const musician1 = await Musician.create({ name: 'Tim', genre: 'country' });
+    
+        expect(musician1.name).toBe('Tim');
+        expect(musician1.genre).toBe('country')
     })
 })
